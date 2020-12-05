44e458fd41e8bf609f69e4a68fc01d5368af9e9b
jbrown7407 <jbrown7407@gmail.com>
2020-12-05 10:43:52 -0500

    api baseline
    baseline

--- models/users.js
+++ models/users.js
@@ -3,4 +3,7 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
   description: { type: String, default: "Best Product ever!" },
   cart: { type: String, required: true },
+  //cart array
+  //recommendations: based on previous purchases
+  // 0auth needed and stripe needed
 });
